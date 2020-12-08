256a36daefe02b8f963c38517aa66dcff2134f61
Dani Bottiger <Bottigerd@carleton.edu>
2020-12-08 09:58:12 -0600

    adding loan math

--- Models/LoanItem.cs
+++ Models/LoanItem.cs
@@ -1,7 +1,11 @@
 public class LoanItem {
-    public long Id { get; set; }
-    public long Balance { get; set; }
-    public long Term { get; set; }
-    public long Rate { get; set; }
+    //input parameters:
+    public int Id { get; set; }
+    public double Balance { get; set; }
+    public int Term { get; set; }
+    public double Rate { get; set; }
+
+    //calculated parameters:
+    public double monthlyPayment { get; set; } //When this is assigned wherever, call the Monthly payment function from LoanMath.cs
 
 }

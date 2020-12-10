eeb57846ea4b0a9289314bf9dd0d2b92e6779cfb
Kevin Chen <usa.kevin@gmail.com>
2020-12-09 18:26:45 -0600

    changed loan.service.ts and wrote methods that are supposed to retrieve data from backend but needs more work(based this on pt. 2 of tutorial zz sent). Also added a message service.

--- frontend-sam/src/app/loan.service.ts
+++ frontend-sam/src/app/loan.service.ts
@@ -4,6 +4,7 @@ import { loans } from './mock-loans';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,29 +12,43 @@ import { MessageService } from './message.service';
 
 export class LoanService {
 
-  private loansUrl = 'api/heroes';  // URL to web api
+  appURL: string;  // URL to app
+  loansURL: string;  // URL to web api
 
-  constructor(private http: HttpClient, private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { 
+    this.appURL = environment.appURL;
+    this.loansURL = 'api/heroes';
+  }
 
-  
+  // NEED TO CHANGE, NOT CORRECT!
   getLoans(): Observable<Loan[]> {
     this.messageService.add("LoanService: fetched all loans");
     return of(loans);
   }
   
-  
+  // an attempt at writing methods that get data from backend
   /*
+
   getLoans(): Observable<Loan[]> {
     this.messageService.add("LoanService: fetched all loans");
-    return this.http.get<Loan[]>(this.loansUrl)
+    return this.http.get<Loan[]>(this.appURL + this.loansURL)
+
+  getLoan(id: number): Observable<Loan> {
+      return this.http.get<Loan>(this.appURL + this.loansURL + id);
   }
-  */
-
   
-  // getLoan(id):
-  // addLoan():
-  // deleteLoan(id):
-  // updateLoan(id):
+  saveLoan(Loan): Observable<Loan> {
+      return this.http.post<Loan>(this.appURL + this.loansURL, JSON.stringify(Loan), this.httpOptions);
+  }
 
+  updateLoan(id: number, Loan): Observable<Loan> {
+      return this.http.put<Loan>(this.appURL + this.loansURL + id, JSON.stringify(Loan), this.httpOptions);
+  }
+
+  deleteLoan(id: number): Observable<Loan> {
+      return this.http.delete<Loan>(this.appURL + this.loansURL + id);
+  }
+
+  */
 
 }

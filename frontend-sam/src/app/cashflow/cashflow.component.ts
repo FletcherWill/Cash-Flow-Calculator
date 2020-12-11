b83199f59bec130794811f5c2c9d98714ebaa428
masgrebnesor <llsamrj@gmail.com>
2020-12-11 10:59:20 -0800

    Added cashflow table support

--- frontend-sam/src/app/cashflow/cashflow.component.ts
+++ frontend-sam/src/app/cashflow/cashflow.component.ts
@@ -11,13 +11,23 @@ import { Loan } from '../loan';
 })
 export class CashflowComponent implements OnInit {
 
-
+  cashflows: cashflowChart[] = [];
+  loans: Loan[] = [];
 
   constructor(private loanService: LoanService) { }
 
   ngOnInit(): void {
   }
 
+  getCashflows(): void {
+    this.loanService.getLoans()
+        .subscribe(loans => this.loans = loans);
+
+    for (const loan of this.loans)
+    {
+      this.cashflows.push(this.getLoanChart(loan))
+    }
+  }
 
   getLoanChart(loan: Loan): void {
     this.loanService.getLoanChart(loan)
